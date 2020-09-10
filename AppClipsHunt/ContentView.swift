edaaf775d229d93b3d54320ee6ad69c5e4ad8d09
Magnus <hei@purpl.no>
2020-09-10 10:40:58 +0200

    Initial Commit

---
+++ AppClipsHunt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppClipsHunt
+//
+//  Created by Magnus Tviberg on 10/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
